e996248f042ef3144645032277511b28c2b3e896
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-08-26 08:20:00 +0200

    Overrriden save maethod for Book model and added slug to the url

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,18 +1,21 @@
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404, render
 from .models import Book
-
+from django.db.models import Avg, Max, Min
 # Create your views here.
 
 
 def index(request):
+    books = Book.objects.all().order_by("title")
     return render(request, "book_outlet/index.html", {
-        "books": Book.objects.all()
+        "books": books,
+        "total_number_of_books": books.count(),
+        "average_rating": books.aggregate(Avg("rating"))
     })
 
 
-def book_detail(request, id: int):
-    book = get_object_or_404(Book, pk=id)
+def book_detail(request, slug: str):
+    book = get_object_or_404(Book, slug=slug)
     
     return render(request, "book_outlet/book_detail.html", {
         "book": book
