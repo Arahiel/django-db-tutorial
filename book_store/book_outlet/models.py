283c3784710e4ec38e3ad98d49a382941e901646
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-08-27 09:21:25 +0200

    -Added Many-To-One relation (Books-Author) -Added One-To-One relation (Author-Address) -Added Many-To-Many relation (Books-Countries)
    -Added Meta classes to change the display of Address and Country models

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,14 +4,51 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=7)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.full_address()
+    
+    def full_address(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Addresses"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.full_name()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country, related_name="books")
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
